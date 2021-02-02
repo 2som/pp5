e56d86a089f8ea940bf547d074455de0733bf831
Maks Świerczyński <maksswierczynski@gmail.com>
2021-02-02 18:39:19 +0100

    cleanup

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+Create database IF NOT EXISTS pp5;
+
 CREATE TABLE IF NOT EXISTS product_catalog__products (
     id VARCHAR(255) NOT NULL primary key,
     description TEXT,
--- src/main/java/pl/mswierczynski/pp5/sales/PaymentUpdateStatusRequest.java
+++ src/main/java/pl/mswierczynski/pp5/sales/PaymentUpdateStatusRequest.java
@@ -2,5 +2,6 @@ package pl.mswierczynski.pp5.sales;
 
 public class PaymentUpdateStatusRequest {
     public static PaymentUpdateStatusRequest of(String signatureHeader, String body) {
+        return null;
     }
 }
